07326b0f576f12405ee8d26c08085ddde569b589
bryancarrillo1 <carrbryan0123@gmail.com>
2024-12-02 17:10:32 -0500

    added comparators for memberdetails

--- castmember.py
+++ castmember.py
@@ -3,3 +3,11 @@ class MemberDetails:
         # Name is a basic string and the movies is going to be an array of strings
         self.name = name
         self.movies = movies
+
+    def __eq__(self, other):
+        # checks if names are equal
+        return self.name == other.name
+
+    def __lt__(self, other):
+        # checks if name is less than other name lexicographically
+        return self.name < other.name
