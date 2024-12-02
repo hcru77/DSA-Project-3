58d620632d754e7036783e8cc6b0aa15762a969b
Deepug116 <89586741+Deepug116@users.noreply.github.com>
2024-12-02 15:37:43 -0500

    red black tree structure

---
+++ redblacktree.py
@@ -0,0 +1,107 @@
+# class to implement node of RB Tree
+class RBNode:
+        # constructor
+    def __init__(self, value, color='red'):
+        self.value = value
+        self.color = color
+        self.left = None
+        self.right = None
+        self.parent = None
+
+    # function to get the grandparent of node
+    def grandparent(self):
+        if self.parent is None:
+            return None
+        return self.parent.parent
+
+    # function to get the sibling of node
+    def sibling(self):
+        if self.parent is None:
+            return None
+        if self == self.parent.left:
+            return self.parent.right
+        return self.parent.left
+
+    # function to get the uncle of node
+    def uncle(self):
+        if self.parent is None:
+            return None
+        return self.parent.sibling()
+
+# function to implement Red Black Tree
+
+
+class RedBlackTree:
+        # constructor to initialize the RB tree
+    def __init__(self):
+        self.root = None
+
+    # function to search a value in RB Tree
+    def search(self, value):
+        curr_node = self.root
+        while curr_node is not None:
+            if value == curr_node.value:
+                return curr_node
+            elif value < curr_node.value:
+                curr_node = curr_node.left
+            else:
+                curr_node = curr_node.right
+        return None
+
+    # function to insert a node in RB Tree, similar to BST insertion
+    def insert(self, value):
+        # Regular insertion
+        new_node = RBNode(value)
+        if self.root is None:
+            self.root = new_node
+        else:
+            curr_node = self.root
+            while True:
+                if value < curr_node.value:
+                    if curr_node.left is None:
+                        curr_node.left = new_node
+                        new_node.parent = curr_node
+                        break
+                    else:
+                        curr_node = curr_node.left
+                else:
+                    if curr_node.right is None:
+                        curr_node.right = new_node
+                        new_node.parent = curr_node
+                        break
+                    else:
+                        curr_node = curr_node.right
+        self.insert_fix(new_node)
+
+    # Function to fix RB tree properties after insertion
+    def insert_fix(self, new_node):
+        while new_node.parent and new_node.parent.color == 'red':
+            if new_node.parent == new_node.grandparent().left:
+                uncle = new_node.uncle()
+                if uncle and uncle.color == 'red':
+                    new_node.parent.color = 'black'
+                    uncle.color = 'black'
+                    new_node.grandparent().color = 'red'
+                    new_node = new_node.grandparent()
+                else:
+                    if new_node == new_node.parent.right:
+                        new_node = new_node.parent
+                        self.rotate_left(new_node)
+                    new_node.parent.color = 'black'
+                    new_node.grandparent().color = 'red'
+                    self.rotate_right(new_node.grandparent())
+            else:
+                uncle = new_node.uncle()
+                if uncle and uncle.color == 'red':
+                    new_node.parent.color = 'black'
+                    uncle.color = 'black'
+                    new_node.grandparent().color = 'red'
+                    new_node = new_node.grandparent()
+                else:
+                    if new_node == new_node.parent.left:
+                        new_node = new_node.parent
+                        self.rotate_right(new_node)
+                    new_node.parent.color = 'black'
+                    new_node.grandparent().color = 'red'
+                    self.rotate_left(new_node.grandparent())
+        self.root.color = 'black'
